f1f02125da3512d60c8c2498cf17a25cbd64204d
ShoGinn <scottginn@Mac-mini.home>
2019-04-19 16:57:09 -0700

    Update fr24dl.sh

--- fr24dl.sh
+++ fr24dl.sh
@@ -6,7 +6,7 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 
-if [[ $DOCKER_ARCH =~ arm ]] ; then
+if [[ $ARCH =~ arm ]] ; then
 	echo "Download armhf version"
 	curl --output fr24feed.tgz "http://repo.feed.flightradar24.com/rpi_binaries/fr24feed_${1}_armhf.tgz" && \
     sha256sum fr24feed.tgz && echo "${2}  fr24feed.tgz" | sha256sum -c && \
@@ -14,7 +14,7 @@ if [[ $DOCKER_ARCH =~ arm ]] ; then
     mv fr24feed /usr/bin/fr24feed && \
     rm fr24feed.tgz
 fi
-if [ "$DOCKER_ARCH" = "amd64" ] ; then
+if [ "$ARCH" = "amd64" ] ; then
 	echo "Download AMD64 version"
 	curl --output fr24feed.tgz "https://repo-feed.flightradar24.com/linux_x86_64_binaries/fr24feed_${3}_amd64.tgz" && \
 	sha256sum fr24feed.tgz && echo "${4}  fr24feed.tgz" | sha256sum -c && \
