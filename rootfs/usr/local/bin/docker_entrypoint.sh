f565b62c53cf6389aaf973e63acb57ab153df12f
ShoGinn <2660603+ShoGinn@users.noreply.github.com>
2019-05-01 06:50:32 -0700

    new

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -3,8 +3,8 @@
 set -o errexit          # Exit on most errors (see the manual)
 #set -o errtrace         # Make sure any error trap is inherited
 set -o nounset          # Disallow expansion of unset variables
-#set -o pipefail         # Use last non-zero exit code in a pipeline
-set -o xtrace          # Trace the execution of the script (debug)
+set -o pipefail         # Use last non-zero exit code in a pipeline
+#set -o xtrace          # Trace the execution of the script (debug)
 
 
 echo "Waiting for dump1090 to start up"
@@ -31,5 +31,5 @@ nslookup feed.flightradar24.com
 ping -c 1 feed.flightradar24.com
 echo
 
-fr24feed --config-file=/etc/fr24feed.ini \
+exec fr24feed --config-file=/etc/fr24feed.ini \
            --fr24key="${FR24FEED_KEY}"
