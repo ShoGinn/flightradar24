5269ef55818eb892b99dffaa076d3cd3bc465a95
ShoGinn <2660603+ShoGinn@users.noreply.github.com>
2019-05-01 07:15:14 -0700

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -3,7 +3,7 @@
 set -o errexit          # Exit on most errors (see the manual)
 #set -o errtrace         # Make sure any error trap is inherited
 set -o nounset          # Disallow expansion of unset variables
-set -o pipefail         # Use last non-zero exit code in a pipeline
+#set -o pipefail         # Use last non-zero exit code in a pipeline
 #set -o xtrace          # Trace the execution of the script (debug)
 
 
